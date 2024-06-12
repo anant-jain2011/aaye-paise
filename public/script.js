4854481f210216f478904578300ea37837bd5b5a
anant-jain2011 <kumkumnidhi14@gmail.com>
2024-06-12 17:08:04 +0530

    minor commit

--- public/script.js
+++ public/script.js
@@ -1,18 +1,18 @@
 const colors = ["#ff671f", "#06038d", "#158809"];
 
-document.onload = async () => {
+document.addEventListener("DOMContentLoaded", async () => {
     const res = await fetch("/getblocks");
     const data = await res.json();
 
     data.forEach((mBlock) => {
         const block = document.createElement("div");
-        block.style.backgroundColor = colors[Math.floor(Math.random() * 3 + 1)];
-        block.innerText = mBlock.amount;
+        block.style.backgroundColor = colors[Math.floor(Math.random() * 3)];
+        block.innerText = mBlock.message;
         block.classList.add("block");
 
         document.body.appendChild(block);
-    })
-}
+    });
+});
 
 if (!window.eventSource) {
     window.eventSource = new EventSource("/events");
@@ -20,9 +20,10 @@ if (!window.eventSource) {
     window.eventSource.addEventListener("message", (e) => {
         const block = document.createElement("div");
         block.style.backgroundColor = colors[Math.floor(Math.random() * 3)];
-        block.innerText = e.data;
+        block.innerText = JSON.parse(e.data).message;
         block.classList.add("block");
 
         document.body.appendChild(block);
     });
 }
+
